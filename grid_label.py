bd3b0168f1f82c4ed6590d776797832791857c9c
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2021-10-16 22:25:34 +0600

    basic AI done

---
+++ grid_label.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+class Grid_Label():
+    def __init__(self, master, i, j):
+        self.text = StringVar()
+        self.label = Label(master, textvariable=self.text, height=2, width=7, relief=RIDGE, bg="green", fg="white",
+                           font="Helvetica 14 bold")
+        self.label.grid(row=i, column=j,  sticky=W, pady=1)
+        self.row = i
+        self.col = j
+
+    def change_text(self, updated_text):
+        self.text.set(str(updated_text))
